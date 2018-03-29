d71f166f98bf26207270084f32eb92a510040e43
robcmills <robcmills@gmail.com>
2018-03-28 21:20:41 -0700

    Convert hard coded file and ranks into programmatic expression

--- staex/src/components/board/index.js
+++ staex/src/components/board/index.js
@@ -3,17 +3,22 @@ import React from 'react'
 import Square from '../square/'
 import './board.css'
 
+const BOARD_WIDTH = 3
+const BOARD_HEIGHT = 3
+const fileAndRanks = []
+for (let x = 0; x < BOARD_WIDTH; x++) {
+	for (let y = 0; y < BOARD_HEIGHT; y++) {
+		fileAndRanks.push({ x, y })
+	}
+}
+
 const Board = () =>
 	<div className="board">
-		<Square file={0} rank={0} />
-		<Square file={1} rank={0} />
-		<Square file={2} rank={0} />
-		<Square file={0} rank={1} />
-		<Square file={1} rank={1} />
-		<Square file={2} rank={1} />
-		<Square file={0} rank={2} />
-		<Square file={1} rank={2} />
-		<Square file={2} rank={2} />
+	{
+		fileAndRanks.map(({ x, y }) =>
+			<Square file={x} rank={y} />
+		)
+	}
 	</div>
 
 export default Board
