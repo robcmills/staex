f021a104b243e9c5a853700ef84ede1417f9308c
robcmills <robcmills@gmail.com>
2018-03-31 12:34:55 -0700

    Use select defaultValue

--- staex/src/components/status/action-select/index.js
+++ staex/src/components/status/action-select/index.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 const ActionSelect = ({ activeAction }) =>
-	<select name="action">
-	  <option value="stack" selected={activeAction === 'stack'}>stack</option>
-	  <option value="move" selected={activeAction === 'move'}>move</option>
+	<select name="action" defaultValue={activeAction}>
+	  <option value="stack">stack</option>
+	  <option value="move">move</option>
 	</select>
 
 const mapStateToProps = ({
