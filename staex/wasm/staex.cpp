7163e9891ff49d53595ace27818a13768e73bc0c
robcmills <rob@buildingconnected.com>
2018-06-02 09:58:26 -0700

    Working transfer of player tokens from js bitboard to cpp

--- staex/wasm/staex.cpp
+++ staex/wasm/staex.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #include "staex.h"
 
 extern "C" {
@@ -20,9 +21,21 @@ extern "C" {
 			)
 		);
 
+		int index_length = (board_size * board_size) - 1;
+		int p1_token_index = index_length - log2(p1_token);
+		int p2_token_index = index_length - log2(p2_token);
+		std::cout << "p1_token_index:" << p1_token_index << std::endl;
+		std::cout << "p2_token_index:" << p2_token_index << std::endl;
+
 		for (int y=0; y<board_size; ++y) {
 			for (int x=0; x<board_size; ++x) {
-				board[y][x].height = heights[y * board_size + x];
+				int i = y * board_size + x;
+				board[y][x].height = heights[i];
+				if (i == p1_token_index) {
+					board[y][x].token = 1;
+				} else if (i == p2_token_index) {
+					board[y][x].token = 2;
+				}
 			}
 		}
 
