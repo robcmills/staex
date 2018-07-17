8cafc038349ffb68c51bcd72be490733b84b9763
robcmills <rob@buildingconnected.com>
2018-07-17 18:45:17 -0500

    Show whose move it is in bottom status

--- staex/src/components/status/bottom.js
+++ staex/src/components/status/bottom.js
@@ -2,17 +2,28 @@ import React from 'react'
 import cn from 'classnames'
 
 import magicConnect from '../../redux/magic-connect'
-import { winnerSelector } from '../../redux/selectors'
+import { activePlayerSelector, winnerSelector } from '../../redux/selectors'
 
 import './status.css'
 
-const BottomStatus = ({ winner }) =>
-	<div className={cn('bottom', 'status')}>
-		{!!winner && `${winner === 1 ? 'You win!' : 'Computer wins'}`}
-	</div>
+const BottomStatus = ({ activePlayer, winner }) => {
+	if (winner > 0) {
+		return (
+			<div className={cn('bottom', 'status', `player${winner}Score`)}>
+				{!!winner && `${winner === 1 ? 'You win!' : 'Computer wins'}`}
+			</div>
+		)
+	}
+	return (
+		<div className={cn('bottom', 'status', `player${activePlayer}Score`)}>
+			{activePlayer === 1 ? 'Your move' : 'Computer is thinking...'}
+		</div>
+	)
+}
 
 export default magicConnect({
 	selectors: {
+		activePlayer: activePlayerSelector,
 		winner: winnerSelector,
 	},
 })(BottomStatus)
